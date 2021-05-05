5548d662cff950cf7c11f6f905dd8b6d5d8dd058
Ginner <26798615+Ginner@users.noreply.github.com>
2021-05-05 15:31:12 +0200

    Further work

--- pyinit.sh
+++ pyinit.sh
@@ -5,7 +5,7 @@
 # Initiate a python project using git, pyenv and venv
 # By Morten Ginnerskov
 #
-# Last modified: 2021.05.05-15:12 +0200
+# Last modified: 2021.05.05-15:30 +0200
 #
 # =============================================================== #
 # TODO:
@@ -17,13 +17,10 @@
 PRIVATE="false"
 USER=$( whoami )
 DESCRIPTION=""
-PYTHON_VERSION=$( pyenv versions \
+PYTHON_VERSIONS=$( pyenv versions \
     | awk '{ if( $1 ~ /^[23]\.[[:digit:]]+\.[[:digit:]]+/) print $1 ; else if ( $2 ~ /^[23]\.[[:digit:]]+\.[[:digit:]]+/) print $2 }'
-    | tail -1
 )
-#     | awk '{ if( substr($1,0,1) == "3" && length($1) == 5 ) print $1; else if ( substr($1,0,1) == "*" && length($2) == 5 ) print $2 }' \
-#     | awk '$1 ~ /^[23]\.[[:digit:]]+\.[[:digit:]]+/ { print $1 }; else if ( substr($1,0,1) == "*" )' \
-# Fix to use regex to
+PYTHON_VERSION=$( echo "$PYTHON_VERSIONS" | tail --lines=1 )
 
 # Use environment variable if it is there, otherwise use current directory
 if [[ -n $DEV_PRJ_HOME ]]; then
